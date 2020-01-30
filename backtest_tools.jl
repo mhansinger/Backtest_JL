1bfa34743daff91b58b59b05aaa5515db4447c2c
mhansinger <maximilian.hansinger@gmail.com>
2020-01-30 21:19:52 +0100

    first

---
+++ backtest_tools.jl
@@ -0,0 +1,70 @@
+module backtest_tools
+
+#using Missing
+using DataFrames, CSV, Gadfly, Statistics
+
+
+export get_dataframe, sma, ema, get_crossovers 
+
+# reads the data csv file
+function get_dataframe(path)
+    df = CSV.read(path) 
+    rename!(df,[:x,:Date,:Price])
+    select!(df, Not(:x)) #delete!(df,:x)
+    return df[2:end,:]  #return from 2nd row on
+end
+
+
+function sma(df::DataFrame, window::Int64)
+
+    X = df.Price[:]
+    if window < 1 || length(X) < window
+        return X
+    end
+
+    Y::Array{Union{Float64},1} = zeros(window-1)#missings(window-1)
+
+    for i in window:length(X)
+        push!(Y, mean(X[i-window+1:i]))
+    end
+
+    return Y
+end
+
+function ema(X, window::Int64)
+    if window < 1 || length(X) < window
+        return X
+    end
+
+    L::Array{Union{Float64},1} = zeros(window-1)
+    alpha = 2.0 / (window + 1)
+
+    push!(L, X[window])
+
+    for i in window+1:length(X)
+        push!(L, alpha*X[i] + (1-alpha)*L[end])
+    end
+
+    return L
+end
+
+function get_crossovers(SMA_diff)
+
+    Signals = zeros(length(SMA_diff)); # vector with zeros for BUY and SELL signals
+
+    for i in 2:size(Signals, 1)
+        
+        # if SMA_diff[i] == missing
+        #     continue
+        if SMA_diff[i] > 0 && SMA_diff[i-1] < 0
+            Signals[i] = 1      # BUY signal: short surpasses long
+        elseif SMA_diff[i] < 0 && SMA_diff[i-1] > 0
+            Signals[i] = -1     # SELL signal: short goes below long
+
+        end
+    end
+
+    return Signals
+end
+
+end
---
+++ backtester.jl
@@ -0,0 +1,70 @@
+# dynamic backtest engine
+
+module backtester
+
+include("./backtest_tools.jl")
+using .backtest_tools, DataFrames, Statistics, CSV
+
+export backtest_engine
+
+function backtest_engine(df_in::DataFrame, portfolio::Int64,long::Int64,short::Int64,fee::Float64)
+    df = df_in;
+    df.Portfolio = ones(length(df.Price)) * portfolio;  # set the portfolio vector, initialize with start portfolio
+    df.Market_pos = zeros(length(df.Price));  # 0 means we are not in the market, 1 we are in
+    df.Shares = zeros(length(df.Price));      # number of shares
+
+    # compute the long and short SMA
+    SMA_long = sma(df,long);
+    SMA_short = sma(df,short);
+
+    SMA_diff = SMA_short - SMA_long; # if positiv we should be in the market!
+
+    df.Signals = get_crossovers(SMA_diff); 
+
+    #############
+    # INCLUDE TRADING FEES!!
+    # DONE
+    #############
+
+    # loop over the time series
+    for i in 2:length(df.Signals)
+        if df.Signals[i] == 1 && df.Market_pos[i-1] == 0
+            #println("BUY");
+            #BUY: if Signal says yes and we are not in yet
+            df.Portfolio[i] = df.Portfolio[i-1]*(1-fee);
+            df.Shares[i] = df.Portfolio[i] / df.Price[i];
+            
+            # update Market_pos
+            df.Market_pos[i] = 1; # we are in
+
+        elseif df.Signals[i] == -1 && df.Market_pos[i-1] == 1
+            #println("SELL");
+            #SELL: if Signal says sell your shares and we're still in
+            df.Portfolio[i] = (df.Shares[i-1] * df.Price[i])*(1-fee);
+            df.Shares[i] = 0;
+            # update Market_pos
+            df.Market_pos[i] = 0; # we are out
+
+        elseif df.Signals[i] == 0 && df.Market_pos[i-1] == 0
+            # we are not in the Market and stay out: update Portfolio
+            df.Portfolio[i] = df.Portfolio[i-1];
+            df.Shares[i] = 0;
+            df.Market_pos[i] = 0; # we are out
+            #println("SHORT");
+
+        elseif df.Signals[i] == 0 && df.Market_pos[i-1] == 1
+            # wer are in the Market and stay in
+            df.Shares[i] = df.Shares[i-1];
+            df.Portfolio[i] = df.Shares[i] * df.Price[i];
+            df.Market_pos[i] = 1; # we are in
+            #println("LONG");
+        end
+
+    end
+
+    return df
+
+end
+
+end
+
