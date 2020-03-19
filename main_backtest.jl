90c442fb2a6f03f9f8d64f1bb52a69f56f6dc762
Maximilian Hansinger <maximilian.hansinger@unibw.de>
2020-03-19 22:41:46 +0100

    added heat map plot

--- main_backtest.jl
+++ main_backtest.jl
@@ -1,7 +1,10 @@
 # run the backtester
 
 using Pkg, Plots 
-#Pkg.add("Benchmarks")
+pyplot()    # plotting backend
+Plots.default(overwrite_figure=false)
+
+Pkg.activate(".")
 #using Benchmarks
 include("./backtester.jl")
 include("./backtest_tools.jl")
@@ -9,7 +12,8 @@ include("./backtest_tools.jl")
 using .backtest_tools, .backtester
 
 
-mypath = "/Users/Maxi/Projects/Julia/backtest/XXBTZEUR_Series.csv";
+#mypath = "/Users/Maxi/Projects/Julia/backtest/XXBTZEUR_Series.csv";
+mypath = "./XXBTZEUR_Series.csv";
 
 df = get_dataframe(mypath);
 
@@ -20,8 +24,8 @@ plot(df.Portfolio/df.Portfolio[1],label="Portfolio")
 plot!(df.Price/df.Price[1],label="Price")
 
 
-longs=collect(1000:20:5000)
-shorts = collect(100:10:1000)
+longs=collect(1000:100:5000);
+shorts = collect(100:20:1000);
 
 longs=convert(Array{Int64}, longs)
 
@@ -32,14 +36,25 @@ best_portfolio=0.0;
 best_s = 0;
 best_l = 0;
 
+# store all end_prices to plot a heat map
+global heat_map = zeros((length(longs), length(shorts)));
+
+investment = 1000;
+
+println("Initial investment: ",investment);
+println("With BUY and HODL you have: ",df.Price[end]*investment/df.Price[1]);
+println(" ");
+sleep(3);
+
 a=1;
 
 println("best_portfolio: ",best_portfolio);
 
-for l in longs
-    for s in shorts
-        df_new = backtest_engine(df,1000,l,s,0.005);
+for (l_id, l) in enumerate(longs)
+    for (s_id,s) in enumerate(shorts)
+        df_new = backtest_engine(df,investment,l,s,0.005);
         println(df_new.Portfolio[end])
+        global heat_map[l_id,s_id] = df_new.Portfolio[end];
         if df_new.Portfolio[end] > best_portfolio
             best_portfolio = df_new.Portfolio[end];
             best_s = s;
@@ -47,12 +62,18 @@ for l in longs
         end
     end
 end
-end
+
 
 println(" ");
 println("best_s: ", best_s);
 println("best_l: ", best_l);
 println("best_portfolio: ", best_portfolio);
 
+# plot the best portfolio
+plot(df_new.Portfolio/df_new.Portfolio[1],label="Portfolio",title="Best Portfolio development")
+plot!(df_new.Price/df_new.Price[1],label="Price")
 
 
+heatmap(shorts, longs, heat_map,title="best price heat map",xlabel="short",ylabel="long");
+
+end     # end let
